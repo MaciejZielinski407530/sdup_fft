17d0c9266c627f0d985f0e9e0cc86efd2559d0e9
Piotr Wilkon <sq8vps@gmail.com>
2024-06-16 09:45:24 +0200

    remove old files

--- Projekt_fft/Projekt_fft.srcs/sources_1/new/config.v
+++
@@ -1,5 +0,0 @@
-`define W 20 //fixed-point representation precision
-`define D 10 //decimal point position
-`define SCALE (2**`D)
-`define N 256 //FFT length
-`define r 8 //log2(N)
--- Projekt_fft_pipeline/Projekt_fft.srcs/sources_1/new/config.v
+++
@@ -1,5 +0,0 @@
-`define W 18 //fixed-point representation precision
-`define D 10 //decimal point position
-`define SCALE (2**`D)
-`define N 8 //FFT length
-`define r 3 //log2(N)
